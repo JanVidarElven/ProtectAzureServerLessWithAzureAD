33ccb3c5c56e409405b171af29816da95789b4b8
Jan Vidar Elven <jan.vidar.elven@skill.no>
2022-05-20 13:22:18 +0200

    Updated before Oslo Power Platform and Beyond

--- FunctionApp-AuthMethods-API/GetUserAuthMethods/run.ps1
+++ FunctionApp-AuthMethods-API/GetUserAuthMethods/run.ps1
@@ -15,14 +15,14 @@ If ($AuthHeader) {
 }
 
 # Just some Informational Output for Debugging, remove when not needed
-Write-Host $jwt.scp 
-Write-Host $jwt.roles 
+Write-Host "Calling user is authorized with the following scopes:" $jwt.scp 
+Write-Host "Calling user is authorized with the following roles:" $jwt.roles 
 Write-Host ($jwt.scp -notmatch "Phone.Write")
 Write-Host ($jwt.roles -notcontains "Phone.Write.All")
 
 # Check for user principal name parameter
 If ($Request.Params.userUpn) {
-    $userUpn = $Request.Params.vmName
+    $userUpn = $Request.Params.userUpn
 }
 
 # Check Correct Authorization Scopes and/or Roles
@@ -62,18 +62,57 @@ If (($jwt.scp -notmatch "user_impersonation") -and ($jwt.roles -notcontains "app
         Connect-MgGraph -Scopes $scopes -TenantId $tenantID -ForceRefresh
     }
 
+    # If requesting preview APIs, use beta endpoint
     Select-MgProfile -Name "beta"
             
     If (-Not (Get-Module Microsoft.Graph.Identity.Signins)) { Import-Module Microsoft.Graph.Identity.Signins }
 
+    # Initialize Response Body for value array
+    $responseBody = @{
+        value = @(
+        )
+    }
+
     # Either use ErrorActionPreference or use ErrorAction Stop individually
     #$Global:ErrorActionPreference = 1
+
     try {
         If (($jwt.scp -match "Phone.Read") -or ($jwt.roles -contains "Phone.Read.All")) {
             $userPhoneMethods = Get-MgUserAuthenticationPhoneMethod -UserId $userUpn -ErrorAction Stop
-            $responseBody = $userPhoneMethods | Select-Object Id, PhoneNumber, PhoneType, SmsSignInState | ConvertTo-Json
+            #$responseBody = $userPhoneMethods | Select-Object Id, PhoneNumber, PhoneType, SmsSignInState | ConvertTo-Json
+            $responseBody.value += $userPhoneMethods | Select-Object @{Name = "Type" ; Expression = {"phoneAuthenticationMethod"}}, Id, PhoneNumber, PhoneType, SmsSignInState
             $statusCode = [HttpStatusCode]::OK
         }                
+        If (($jwt.scp -match "Authenticator.Read") -or ($jwt.roles -contains "Authenticator.Read.All")) {         
+            $userAuthenticatorMethods = Get-MgUserAuthenticationMicrosoftAuthenticatorMethod -UserId $userUpn -ErrorAction Stop
+            $responseBody.value += $userAuthenticatorMethods | Select-Object @{Name = "Type" ; Expression = {"microsoftAuthenticatorAuthenticationMethod"}}, Id, displayName, phoneAppVersion, clientAppName, deviceTag
+            $statusCode = [HttpStatusCode]::OK
+        }          
+        If (($jwt.scp -match "Hello.Read") -or ($jwt.roles -contains "Hello.Read.All")) {
+            $userHelloMethods = Get-MgUserAuthenticationWindowHello -UserId $userUpn -ErrorAction Stop
+            $responseBody.value += $userHelloMethods | Select-Object @{Name = "Type" ; Expression = {"windowsHelloForBusinessAuthenticationMethod"}}, Id, displayName, keyStrength
+            $statusCode = [HttpStatusCode]::OK
+        }          
+        If (($jwt.scp -match "Fido2.Read") -or ($jwt.roles -contains "Fido2.Read.All")) {
+            $userFido2Methods = Get-MgUserAuthenticationFido2Method -UserId $userUpn -ErrorAction Stop
+            $responseBody.value += $userFido2Methods | Select-Object @{Name = "Type" ; Expression = {"fido2AuthenticationMethod"}}, Id, displayName, model, attestationCertificates, attestationLevel
+            $statusCode = [HttpStatusCode]::OK
+        }          
+        If (($jwt.scp -match "Email.Read") -or ($jwt.roles -contains "Email.Read.All")) {
+            $userEmailMethods = Get-MgUserAuthenticationEmailMethod -UserId $userUpn -ErrorAction Stop
+            $responseBody.value += $userEmailMethods | Select-Object @{Name = "Type" ; Expression = {"emailAuthenticationMethod"}}, Id, emailAddress
+            $statusCode = [HttpStatusCode]::OK
+        }          
+        If (($jwt.scp -match "Password.Read") -or ($jwt.roles -contains "Password.Read.All")) {
+            $userPasswordMethods = Get-MgUserAuthenticationPasswordMethod -UserId $userUpn -ErrorAction Stop
+            $responseBody.value += $userPasswordMethods | Select-Object @{Name = "Type" ; Expression = {"passwordAuthenticationMethod"}}, Id, password
+            $statusCode = [HttpStatusCode]::OK
+        }          
+        If (($jwt.scp -match "TemporaryAccessPass.Read") -or ($jwt.roles -contains "TemporaryAccessPass.Read.All")) {
+            $userTAPMethods = Get-MgUserAuthenticationTemporaryAccessPassMethod -UserId $userUpn -ErrorAction Stop
+            $responseBody.value += $userTAPMethods | Select-Object @{Name = "Type" ; Expression = {"temporaryAccessPassAuthenticationMethod"}}, Id, startDateTime, lifeTimeInMinutes, isUsableOnce, isUsable
+            $statusCode = [HttpStatusCode]::OK
+        }          
     }
     catch [Microsoft.Graph.PowerShell.Runtime.RestException] {
         $statusCode = [HttpStatusCode]::BadRequest
--- FunctionApp-AuthMethods-API/requirements.psd1
+++ FunctionApp-AuthMethods-API/requirements.psd1
@@ -2,8 +2,8 @@
 # See https://aka.ms/functionsmanageddependency for additional information.
 #
 @{
-    'Az' = '5.*'
+    'Az.Accounts' = '2.7.3'
     'JWTDetails' = '1.*'
-    'Microsoft.Graph.Authentication' = '1.3.1'
-    'Microsoft.Graph.Identity.SignIns' = '1.3.1'
+    'Microsoft.Graph.Authentication' = '1.9.6'
+    'Microsoft.Graph.Identity.SignIns' = '1.9.6'
 }
